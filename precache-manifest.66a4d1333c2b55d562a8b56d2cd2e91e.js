434dd43da40317e50f7012bbd7bad284346039b2
Tomislav Sertic <tsertic5@gmail.com>
2019-08-09 01:32:57 +0200

    Updates

---
+++ precache-manifest.66a4d1333c2b55d562a8b56d2cd2e91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be89106e06ac4c0be12a042cc8afe71",
+    "url": "/tsPortfolio/index.html"
+  },
+  {
+    "revision": "581f44cc89722fe7d3b3",
+    "url": "/tsPortfolio/static/css/main.98870e1b.chunk.css"
+  },
+  {
+    "revision": "0ab5c81455621435436e",
+    "url": "/tsPortfolio/static/js/2.dbc7a474.chunk.js"
+  },
+  {
+    "revision": "581f44cc89722fe7d3b3",
+    "url": "/tsPortfolio/static/js/main.d8b69579.chunk.js"
+  },
+  {
+    "revision": "fb3e401e59376b7ab872",
+    "url": "/tsPortfolio/static/js/runtime~main.41884e75.js"
+  },
+  {
+    "revision": "6a94867f564f22038524d7af9665aa73",
+    "url": "/tsPortfolio/static/media/11.6a94867f.jpg"
+  },
+  {
+    "revision": "01f85c969cae41de550d83a10a4815fe",
+    "url": "/tsPortfolio/static/media/33.01f85c96.jpg"
+  }
+]);
