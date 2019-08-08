8c71cd44801f65a53f0eb1378614b3ae25a50fb8
Tomislav Sertic <tsertic5@gmail.com>
2019-08-09 01:28:42 +0200

    Updates

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+        <Content />
+     
+      
+    </div>
+  );
+}
+
+export default App;
